c6ceaab442f1870d5290e214c4638f79461c5b09
MFaizan <engrfaizan.ai@gmail.com>
2023-08-02 11:28:22 +0500

    modules added in model.py file

--- model.py
+++ model.py
@@ -1,7 +1,13 @@
 '''
 Author: Muhammad Faizan
 -----------------------
-Model script: ...
-code to be added.!
-------------------
+python model.py
 '''
+import torch
+import torch.nn as nn
+import torch.functional as F
+import pytorch_lightning as pl
+from transformers import AutoModel
+from sklearn.metrics import accuracy_score
+
+
