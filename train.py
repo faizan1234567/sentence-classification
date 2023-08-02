ed5348111ecdd348918f7a194588d63952db2dcd
MFaizan <engrfaizan.ai@gmail.com>
2023-08-02 12:54:22 +0500

    added trainer code
    trainer code added

--- train.py
+++ train.py
@@ -5,3 +5,35 @@ from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 
 from dataset import Dataset
 from model import colaModel
+
+
+def main():
+    # instantiate two instance like the dataset and model
+    cola_dataset = Dataset()
+    cola_model = colaModel()
+
+    # add model checkpoint and model early stoppoing callbacks
+    checkpoint_callback = ModelCheckpoint(
+        dir_path = "./models", monitor = "val_loss", mode = "min"
+    )
+
+    early_stoppoing_callback = EarlyStopping(
+        monitor = "val_loss", patience = 3, verbose = True, mode = "min"
+    )
+
+    # now create a trainer object
+    trainer = pl.Trainer(
+        default_root_dir = "logs",
+        gpus = (1 if torch.cuda.is_available() else 0),
+        max_epochs = 5,
+        fast_dev_run = False,
+        logger = pl.loggers.TensorBoardLogger("logs/", name = "cola", version = 1)
+        callbacks = [checkpoint_callback, early_stoppoing_callback]
+    ) 
+
+    # start trainer
+    trainer.fit(cola_model, cola_dataset)
+
+
+if __name__ == "__main__":
+    main()
