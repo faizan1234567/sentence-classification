a4cdf1b64a21eece12903544a2d10fc1e94e2fb9
MFaizan <engrfaizan.ai@gmail.com>
2023-08-04 16:35:08 +0500

    added wandb logger

--- train.py
+++ train.py
@@ -1,11 +1,37 @@
 import torch
 import wandb
+import pandas as pd
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning.loggers import WandbLogger
 from dataset import Dataset
 from model import colaModel
+
+class visualizationLogger(pl.Callback):
+    def __init__(self, dataset):
+        super().__init__()
+        self.dataset = dataset
+    
+    def on_validation_end(self, trainer, pl_module):
+        val_batch = next(iter(self.dataset.val_dataloader()))
+        sentences = val_batch["sentence"]
+
+        outputs = pl_module(val_batch["input_ids"], val_batch["attention_mask"])
+        predictions = torch.argmax(outputs, 1)
+        labels = val_batch["label"]
+
+        df = pd.DataFrame(
+            {"sentence": sentences, "Label": labels.numpy(), "Predictions": predictions.numpy()
+            })
+        wrong_df = df[df["Label"] != df["Predictions"]]
+        trainer.logger.experiment.log(
+            {
+                "examples": wandb.Table(dataframe=wrong_df, allow_mixed_types=True),
+                "global_step": trainer.global_step,
+            }
+        )
+
 
 
 def main():
@@ -22,14 +48,20 @@ def main():
         monitor = "val_loss", patience = 3, verbose = True, mode = "min"
     )
 
+    # adding wandb logger
+    wandb_logger = WandbLogger(project = "MLOps - logging and experiment tracking",
+                               entity = "Faizan")
+    
+
     # now create a trainer object
     trainer = pl.Trainer(
         default_root_dir = "logs",
         gpus = (1 if torch.cuda.is_available() else 0),
+        logger = wandb_logger,
         max_epochs = 5,
         fast_dev_run = False,
         logger = pl.loggers.TensorBoardLogger("logs/", name = "cola", version = 1),
-        callbacks = [checkpoint_callback, early_stoppoing_callback]
+        callbacks = [checkpoint_callback, visualizationLogger(cola_dataset), early_stoppoing_callback]
     ) 
 
     # start trainer
