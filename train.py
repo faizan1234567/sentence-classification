d0272466d14f93e8e05bd69d9f8e125c2d99dde5
MFaizan <engrfaizan.ai@gmail.com>
2023-08-04 16:20:59 +0500

    added wandb logger to training file

--- train.py
+++ train.py
@@ -1,8 +1,9 @@
 import torch
+import wandb
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
-
+from pytorch_lightning.loggers import WandbLogger
 from dataset import Dataset
 from model import colaModel
 
