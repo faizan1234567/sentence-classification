cc5e5ce3b0ec6bcba29cf9f83f6a43d0c2de8d0c
MFaizan <engrfaizan.ai@gmail.com>
2023-08-06 17:39:28 +0500

    added hydra dependencies

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import torch
 import wandb
 import pandas as pd
 import pytorch_lightning as pl
+import hydra
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning.loggers import WandbLogger
