c532c075d41becc90c794199e4407252332f2022
MFaizan <mohammadfaizantanoli@gmail.com>
2023-08-07 14:54:58 +0500

    logger added in train.py
    added logger, both file and stream handler

--- train.py
+++ train.py
@@ -12,6 +12,16 @@ from model import colaModel
 import logging
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler(filename= "logs/logger.log")
+stream_handler = logging.StreamHandler()
+formatter = logging.Formatter(fmt= "%(asctime)s: %(message)s", datefmt= '%Y-%m-%d %H:%M:%S')
+file_handler.setFormatter(formatter)
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 
 class visualizationLogger(pl.Callback):
     def __init__(self, dataset):
