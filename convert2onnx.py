62a58229849df7784f690d7a485bad7076de8306
MFaizan <mohammadfaizantanoli@gmail.com>
2023-09-24 14:38:46 +0500

    updated onnx file

--- convert2onnx.py
+++ convert2onnx.py
@@ -17,3 +17,19 @@ from dataset import Dataset
 # configure logger
 logger = logging.getLogger(__name__)
 
+# convert to onnx foramt
+@hydra.main(config_path="configs/", config_name='configs.yaml', version_base= None)
+def convert(cfg):
+    root_dir = hydra.utils.get_original_cwd()
+    model_path = f"{root_dir}/models/best-checkpoint.ckpt"
+    logger.info(f"loading pretrained model from {model_path}")
+    model = colaModel.load_from_checkpoint(model_path)
+
+    data = Dataset(model=cfg.model.tokenizer, batch_size= cfg.preprocess.batch_size,
+                   max_length= cfg.prprocess.max_length)
+    
+    
+
+
+if __name__ == '__main__':
+    convert()
