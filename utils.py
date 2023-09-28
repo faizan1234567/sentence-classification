c6f639f22648c980781642eb58ef4de732e926af
unknown <mohammadfaizantanoli@gmail.com>
2023-09-28 15:47:03 +0500

    updated utils for inference time

---
+++ utils.py
@@ -0,0 +1,20 @@
+import time
+from functools import wraps
+
+def timing(f):
+    """
+    timing function decorator
+    usage: 
+    @timing
+    def function(a):
+       pass
+    """
+
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = f(*args, **kwargs)
+        end = time.time()
+        print("time:%r taken: %2.5f sec" % (f.__name__, end - start))
+        return result
+    return wrapper
